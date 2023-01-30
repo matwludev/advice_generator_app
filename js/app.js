10a39a64e6ad53e02892e5d1b532579499c2de0d
Mateusz Włusek <mateusz.wlusek@gmail.com>
2023-01-30 17:40:41 +0100

    Added tablet view changes

--- js/app.js
+++ js/app.js
@@ -3,13 +3,21 @@ const adviceNum = document.querySelector(".advice_num");
 const generateBtn = document.querySelector("#generate");
 
 const adviceGen = async () => {
-	const advice = await fetch("https://api.adviceslip.com/advice", {
-		cache: "no-cache",
-	});
-	const adviceJson = await advice.json();
-	console.log(adviceJson);
-	adviceNum.textContent = adviceJson.slip.id;
-	adviceTxt.textContent = '"' + adviceJson.slip.advice + '"';
+	try {
+		const advice = await fetch("https://api.adviceslip.com/advice", {
+			cache: "no-cache",
+		});
+
+		if (!advice.ok) {
+			alert("Api problems. Try again later");
+		} else {
+			const adviceJson = await advice.json();
+			adviceNum.textContent = adviceJson.slip.id;
+			adviceTxt.textContent = '"' + adviceJson.slip.advice + '"';
+		}
+	} catch (e) {
+		alert(e);
+	}
 };
 
 window.addEventListener("load", adviceGen);
